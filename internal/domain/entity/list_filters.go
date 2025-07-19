54b42ffa61258ab704f2d9b293174248c5a340ca
Rafiur <42866452+Rafiur@users.noreply.github.com>
2025-07-20 00:00:40 +0600

    expense category repo and usecase layer added

--- internal/domain/entity/list_filters.go
+++ internal/domain/entity/list_filters.go
@@ -5,3 +5,9 @@ type FilterAccountListRequest struct {
 	Name   string `json:"name"`
 	UserID string `json:"user_id"`
 }
+
+type FilterExpenseCategoryListRequest struct {
+	ID               string `json:"id"`
+	Name             string `json:"name"`
+	ParentCategoryID string `json:"parent_category_id"`
+}
---
+++ internal/usecase/expense_category.go
@@ -0,0 +1,34 @@
+package usecase
+
+import (
+	"context"
+	"github.com/Rafiur/wallet_app/internal/domain/entity"
+	"github.com/Rafiur/wallet_app/internal/infrastructure/repository"
+	"github.com/Rafiur/wallet_app/internal/infrastructure/repository/schema"
+)
+
+type ExpenseCategoryService struct {
+	ExpenseCategoryRepo repository.ExpenseCategoryRepoInterface
+}
+
+func NewExpenseCategoryService(expenseCategoryRepo repository.ExpenseCategoryRepoInterface) *ExpenseCategoryService {
+	return &ExpenseCategoryService{
+		ExpenseCategoryRepo: expenseCategoryRepo,
+	}
+}
+
+func (svc *ExpenseCategoryService) Create(ctx context.Context, req *schema.ExpenseCategory) (*schema.ExpenseCategory, error) {
+	return svc.ExpenseCategoryRepo.Create(ctx, req)
+}
+
+func (svc *ExpenseCategoryService) GetByID(ctx context.Context, id string) (*schema.ExpenseCategory, error) {
+	return svc.ExpenseCategoryRepo.GetByID(ctx, id)
+}
+
+func (svc *ExpenseCategoryService) GetAll(ctx context.Context, filter *entity.FilterExpenseCategoryListRequest) ([]*schema.ExpenseCategory, error) {
+	return svc.ExpenseCategoryRepo.List(ctx, filter)
+}
+
+func (svc *ExpenseCategoryService) Update(ctx context.Context, req *schema.ExpenseCategory) (*schema.ExpenseCategory, error) {
+	return svc.ExpenseCategoryRepo.Update(ctx, req)
+}
